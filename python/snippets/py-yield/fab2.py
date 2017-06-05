6fbabbab14092845217c86cda4f4d1289e22832f
root <checkking@foxmail.com>
2017-06-05 14:24:14 +0800

    c

---
+++ python/snippets/py-yield/fab2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+def fab(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b
+        a, b = b, a + b
+        n = n + 1
+
+if __name__ == '__main__':
+    for n in fab(10):
+        print n
